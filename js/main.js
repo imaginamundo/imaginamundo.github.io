f519b80defd8ee4269700d3037cf775898de731f
Diogo Fernandes <diogos.go@gmail.com>
2017-01-21 16:59:09 -0200

    Add media tags and change audio interaction

--- js/main.js
+++ js/main.js
@@ -4,20 +4,24 @@
     // music
     var music = document.getElementById('story-music-box');
     document.getElementById('play-a-song-for-me').addEventListener('click', function(e) {
-        if (this.classList.contains('icon-pause')) {
+        if (music.paused) {
             music.pause();
-            this.classList.remove('icon-pause');
-            this.classList.add('icon-play');
         }
         else {
             music.play();
-            this.classList.remove('icon-play');
-            this.classList.add('icon-pause');
         }
     });
 
     music.onended = function() {
         document.getElementById('play-a-song-for-me').classList.remove('icon-play', 'icon-pause');
         document.getElementById('play-a-song-for-me').classList.add('icon-pause');
+    };
+    music.onplay = function() {
+        document.getElementById('play-a-song-for-me').classList.remove('icon-play', 'icon-pause');
+        document.getElementById('play-a-song-for-me').classList.add('icon-pause');
+    };
+    music.onpause = function() {
+        document.getElementById('play-a-song-for-me').classList.remove('icon-play', 'icon-pause');
+        document.getElementById('play-a-song-for-me').classList.add('icon-play');
     };
 })();
